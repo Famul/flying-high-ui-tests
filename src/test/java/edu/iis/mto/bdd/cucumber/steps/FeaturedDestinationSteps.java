fb4ebd5c1018ea868f5389f22830ba9e6fddc3ae
Famulski.Michal <187684@edu.p.lodz.pl>
2018-06-05 17:22:08 +0200

    Used enum type in place of user in test steps

--- src/test/java/edu/iis/mto/bdd/cucumber/steps/FeaturedDestinationSteps.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/steps/FeaturedDestinationSteps.java
@@ -1,11 +1,15 @@
 package edu.iis.mto.bdd.cucumber.steps;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import edu.iis.mto.bdd.cucumber.pages.Pages;
 
 /**
  * A description goes here. User: john Date: 6/10/13 Time: 9:13 PM
@@ -18,6 +22,17 @@ public class FeaturedDestinationSteps {
     public void init() {
         driver = new FirefoxDriver();
     }
+
+    @Given("^(.*) has logged on$")
+    public void aUserHasLoggedOnAs(String user) {
+        driver.get(Pages.LOGIN_PAGE);
+        driver.findElement(By.name("email")).sendKeys("janina.kowalska@acme.com");
+        driver.findElement(By.name("password")).sendKeys("s3cr3t");
+        driver.findElement(By.name("signin")).click();
+    }
+
+    @When("^(?:.*) views the home page$")
+    public void whenAUserViewsTheHomePage() {}
 
     @Then("^(?:she|he) should see ([0-9]+) featured destinations$")
     public void shouldSeeFeaturedDestinations(int featuredCount) {
--- src/test/java/edu/iis/mto/bdd/cucumber/steps/UserAuthenticationSteps.java
+++ src/test/java/edu/iis/mto/bdd/cucumber/steps/UserAuthenticationSteps.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertThat;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
@@ -13,42 +15,37 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import edu.iis.mto.bdd.cucumber.pages.Pages;
+import edu.iis.mto.bdd.model.FrequentFlyerMember;
 
 public class UserAuthenticationSteps {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @Before
     public void init() {
         driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
     }
 
     @Given("^(.*) is a registered Frequent Flyer$")
     public void givenARegisteredFrequentFlyer(String userEmail) {}
 
     @When("^(.*) authenticates with a valid email address and password$")
-    public void whenJaneAuthenticatesWithAValidEmailAddressAndPassword(String user) {
+    public void whenJaneAuthenticatesWithAValidEmailAddressAndPassword(FrequentFlyerMember user) {
         driver.get(Pages.LOGIN_PAGE);
-        driver.findElement(By.name("email")).sendKeys("janina.kowalska@acme.com");
-        driver.findElement(By.name("password")).sendKeys("s3cr3t");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email")));
+        driver.findElement(By.name("email")).sendKeys(user.getEmail());
+        driver.findElement(By.name("password")).sendKeys(user.getPassword());
         driver.findElement(By.name("signin")).click();
     }
 
     @Then("^(.*) should be given access to (?:her|his) account$")
-    public void thenTheUserShouldBeGivenAccessToAccount(String userName) {
-        assertThat(driver.findElement(By.id("welcome-message")).getText(), equalTo("Witaj Janina"));
-    }
+    public void thenTheUserShouldBeGivenAccessToAccount(FrequentFlyerMember user) {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("welcome-message")));
 
-    @Given("^(.*) has logged on$")
-    public void aUserHasLoggedOnAs(String user) {
-        driver.get(Pages.LOGIN_PAGE);
-        driver.findElement(By.name("email")).sendKeys("janina.kowalska@acme.com");
-        driver.findElement(By.name("password")).sendKeys("s3cr3t");
-        driver.findElement(By.name("signin")).click();
+        assertThat(driver.findElement(By.id("welcome-message")).getText(), equalTo("Witaj " + user.getFirstName()));
     }
-
-    @When("^(?:.*) views the home page$")
-    public void whenAUserViewsTheHomePage() {}
 
     @After
     public void close() {
